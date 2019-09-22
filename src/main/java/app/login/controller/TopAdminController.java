4a577625b8ffb1055559e447b32524b2304c03f8
a-pompom <aoi.matsuda.3x@gmail.com>
2019-09-22 19:51:48 +0900

    管理者トップ画面のテストコード作成
    利用者トップ画面と基本的には同じ構造だったので、
    そのまま流用する形でテストコードを作成・検証を行った
    
    #9

--- src/main/java/app/login/controller/TopAdminController.java
+++ src/main/java/app/login/controller/TopAdminController.java
@@ -1,6 +1,9 @@
 package app.login.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,7 +19,8 @@ public class TopAdminController {
 	 * @return 管理者用TOPページのビュー
 	 */
 	@RequestMapping("/top_admin/init")
-	private String init() {
+	private String init(Model model, @AuthenticationPrincipal UserDetails loginUser) {
+		model.addAttribute("loginUsername", loginUser.getUsername());
 		return "top_admin";
 	}
 
---
+++ src/test/java/app/login/test/controller/TopAdminControllerTest.java
@@ -0,0 +1,61 @@
+package app.login.test.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+
+import app.login.main.LoginUtApplication;
+import app.login.test.util.CsvDataSetLoader;
+import app.login.test.util.WithMockCustomUser;
+
+@ExtendWith(SpringExtension.class)
+@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+@TestExecutionListeners({
+	  DependencyInjectionTestExecutionListener.class,
+	  TransactionalTestExecutionListener.class,
+	  DbUnitTestExecutionListener.class,
+	  WithSecurityContextTestExecutionListener.class
+	})
+@AutoConfigureMockMvc
+@SpringBootTest(classes = {LoginUtApplication.class})
+@Transactional
+public class TopAdminControllerTest {
+	
+	//mockMvc TomcatサーバへデプロイすることなくHttpリクエスト・レスポンスを扱うためのMockオブジェクト
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	@DatabaseSetup(value = "/controller/top/setUp/")
+	@WithMockCustomUser(username="admin_user", password="password")
+	void init処理でviewとしてユーザトップが渡される() throws Exception {
+		this.mockMvc.perform(get("/top_admin/init"))
+			.andExpect(view().name("top_admin"));
+	}
+	
+	@Test
+	@DatabaseSetup(value = "/controller/top/setUp/")
+	@WithMockCustomUser(username="admin_user", password="password")
+	void init処理でログインユーザ名がモデルへ渡される() throws Exception {
+		this.mockMvc.perform(get("/top_admin/init"))
+			.andExpect(model().attribute("loginUsername", "admin_user"));
+	}
+
+}
